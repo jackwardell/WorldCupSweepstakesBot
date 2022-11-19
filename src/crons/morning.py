e6049f2e6de1a2defd5c08259558742c4bce0795
Jack <jack@wardell.xyz>
2022-11-19 17:21:16 +0000

    weather 3

--- src/crons/morning.py
+++ src/crons/morning.py
@@ -1,14 +1,20 @@
+from src.shared.open_weather_map_api.api import get_open_weather_map_api
 from src.shared.rapid_api.api import get_football_api
 from src.shared.telegram_api.api import get_telegram_api
 
 if __name__ == "__main__":
     football_api = get_football_api()
     telegram_api = get_telegram_api()
+    weather_api = get_open_weather_map_api()
+
+    weather_emoji = weather_api.get_weather_in_peckham().emoji
+    good_morning_message = f'{weather_emoji} Good Morning Friends {weather_emoji}'
+    telegram_api.send_message(good_morning_message)
 
     fixtures = football_api.get_fixtures()
 
     if fixtures:
-        starting_message = "Here are the fixtures today 👇"
+        starting_message = f"Today there are {len(fixtures)} matches. Here are the fixtures today 👇"
         telegram_api.send_message(starting_message)
         for fixture in fixtures:
             print(fixture.morning_message)
--- src/shared/open_weather_map_api/api.py
+++ src/shared/open_weather_map_api/api.py
@@ -1,9 +1,19 @@
+from __future__ import annotations
+
+from functools import lru_cache
+
 import attr
 import requests
+
 from src.shared.config import get_config
 from src.shared.open_weather_map_api.domain import Weather
 
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+@lru_cache
+def get_open_weather_map_api() -> OpenWeatherMapApi:
+    return OpenWeatherMapApi()
 
 
 @attr.s

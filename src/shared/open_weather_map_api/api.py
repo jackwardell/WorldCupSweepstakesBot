2c9d1f1df25dabbd6785760965ba92022e176927
Jack <jack@wardell.xyz>
2022-11-19 17:15:49 +0000

    weather 2

--- src/shared/open_weather_map_api/api.py
+++ src/shared/open_weather_map_api/api.py
@@ -1,27 +1,21 @@
 import attr
 import requests
 from src.shared.config import get_config
+from src.shared.open_weather_map_api.domain import Weather
 
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
-
-
 
 
 @attr.s
 class OpenWeatherMapApi:
     api_key = attr.ib(factory=lambda: get_config().OPEN_WEATHER_MAP_API_KEY)
 
-    def get_weather_in_peckham(self) -> str:
-        weather_id = requests.get(
+    def get_weather_in_peckham(self) -> Weather:
+        response = requests.get(
             WEATHER_URL,
             params={
                 "q": "Peckham",
                 "appid": self.api_key
             }
-        ).json()
-        # ["weather"][0]["id"]
-        from pprint import pprint
-        pprint(weather_id)
-
-
-OpenWeatherMapApi().get_weather_in_peckham()
+        )
+        return Weather.from_response(response.json())

cf5cea23d4af3876d07206de9e2488c4de0620fe
Jack <jack@wardell.xyz>
2022-11-22 23:34:37 +0000

    pyhton3

--- src/init.sh
+++ src/init.sh
@@ -1,4 +1,4 @@
-python ./crons/ingest_participants.py &&
-python ./crons/ingest_teams.py &&
-python ./crons/ingest_team_and_participants.py &&
-python ./crons/ingest_fixtures.py
+python3 ./crons/ingest_participants.py &&
+python3 ./crons/ingest_teams.py &&
+python3 ./crons/ingest_team_and_participants.py &&
+python3 ./crons/ingest_fixtures.py

c0f915abb0a7c222ae941a876ca61c610e1cf47a
Jack <jack@wardell.xyz>
2022-11-30 00:25:41 +0000

    better tests

--- src/tests/test_shared/test_bot_api/test_api.py
+++ src/tests/test_shared/test_bot_api/test_api.py
@@ -4,6 +4,8 @@ from unittest.mock import MagicMock
 import pytest
 from src.shared.bot_api.api import BotApi
 from src.shared.bot_api.db import TeamDrawnByParticipantORM
+from src.shared.bot_api.models import Participant
+from src.shared.bot_api.models import Team
 from src.shared.football_api.models import FootballTeam
 from src.shared.telegram_api.models import TelegramParticipant
 
@@ -14,42 +16,52 @@ def bot_api() -> BotApi:
 
 
 @pytest.mark.parametrize(
-    "telegram_participants",
+    "input_telegram_participants, output_participants",
     [
-        [
-            TelegramParticipant(telegram_user_id=69, first_name="Rick"),
-        ],
-        [
-            TelegramParticipant(telegram_user_id=69, first_name="Rick"),
-            TelegramParticipant(telegram_user_id=420, first_name="Morty"),
-        ],
+        (
+            [TelegramParticipant(telegram_user_id=69, first_name="Rick")],
+            [Participant(telegram_user_id=69, first_name="Rick")],
+        ),
+        (
+            [
+                TelegramParticipant(telegram_user_id=69, first_name="Rick"),
+                TelegramParticipant(telegram_user_id=420, first_name="Morty"),
+            ],
+            [
+                Participant(telegram_user_id=69, first_name="Rick"),
+                Participant(telegram_user_id=420, first_name="Morty"),
+            ],
+        ),
     ],
 )
-def test_save_and_get_participant(bot_api: BotApi, telegram_participants: List[TelegramParticipant]) -> None:
-    for telegram_participant in telegram_participants:
+def test_save_and_get_participant(
+    bot_api: BotApi,
+    input_telegram_participants: List[TelegramParticipant],
+    output_participants: List[Participant],
+) -> None:
+    for telegram_participant in input_telegram_participants:
         bot_api.save_participant(telegram_participant)
     participants = bot_api.get_participants()
-    assert [i.dict() for i in telegram_participants] == [i.dict() for i in participants]
+    assert output_participants == participants
 
 
 @pytest.mark.parametrize(
-    "football_teams",
+    "input_football_teams, output_teams",
     [
-        [
-            FootballTeam(football_api_id=1, name="England"),
-        ],
-        [
-            FootballTeam(football_api_id=1, name="England"),
-            FootballTeam(football_api_id=2, name="France"),
-        ],
+        ([FootballTeam(football_api_id=1, name="England")], [Team(football_api_id=1, name="England")]),
+        (
+            [FootballTeam(football_api_id=1, name="England"), FootballTeam(football_api_id=2, name="France")],
+            [Team(football_api_id=1, name="England"), Team(football_api_id=2, name="France")],
+        ),
     ],
 )
-def test_save_and_get_team(bot_api: BotApi, football_teams: List[FootballTeam]) -> None:
-    for football_team in football_teams:
+def test_save_and_get_team(
+    bot_api: BotApi, input_football_teams: List[FootballTeam], output_teams: List[Team]
+) -> None:
+    for football_team in input_football_teams:
         bot_api.save_team(football_team)
     teams = bot_api.get_teams()
-    # exclude_none as we don't want {'participant': None} in the dict
-    assert [i.dict() for i in football_teams] == [i.dict(exclude_none=True) for i in teams]
+    assert output_teams == teams
 
 
 def test_save_team_drawn_by_participant(bot_api: BotApi) -> None:

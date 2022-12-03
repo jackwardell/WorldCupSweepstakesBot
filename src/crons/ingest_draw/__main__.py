63425ceae9f4496826f41e1083af2e8e54ed92f3
Jack <jack@wardell.xyz>
2022-12-03 13:17:43 +0000

    fix ingest_draw

--- src/crons/ingest_draw/__main__.py
+++ src/crons/ingest_draw/__main__.py
@@ -1,41 +1,6 @@
 from src.shared.bot_api.api import BotApiError
 from src.shared.bot_api.api import get_bot_api
 
-# DRAW = {
-#     "Argentina": "Paddy",
-#     "Australia": "Alex",
-#     "Belgium": "Sam",
-#     "Brazil": "Zoe",
-#     "Cameroon": "Zoe",
-#     "Canada": "Nathalie",
-#     "Costa Rica": "Jack",
-#     "Croatia": "Jack",
-#     "Denmark": "Delia",
-#     "Ecuador": "Moya",
-#     "England": "Moya",
-#     "France": "Theo",
-#     "Germany": "Lucy",
-#     "Ghana": "Ludo",
-#     "Iran": "Giulia",
-#     "Japan": "Hannah",
-#     "Mexico": "Ludo",
-#     "Morocco": "Paddy",
-#     "Netherlands": "Hannah",
-#     "Poland": "Benjamin",
-#     "Portugal": "Benjamin",
-#     "Qatar": "Emma",
-#     "Saudi Arabia": "Theo",
-#     "Senegal": "Lucy",
-#     "Serbia": "Sam",
-#     "South Korea": "Delia",
-#     "Spain": "Nathalie",
-#     "Switzerland": "Alex",
-#     "Tunisia": "Georgia",
-#     "USA": "Giulia",
-#     "Uruguay": "Georgia",
-#     "Wales": "Emma",
-# }
-
 DRAW = {
     26: 1501950617,
     20: 1625076769,

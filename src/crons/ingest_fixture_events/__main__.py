09d65185f17638eeb78ef18b046accb6f49fa383
Jack <jack@wardell.xyz>
2022-12-03 15:21:09 +0000

    ingest fixture events skeleton

---
+++ src/crons/ingest_fixture_events/__main__.py
@@ -0,0 +1,17 @@
+from src.shared.bot_api.api import BotApiError
+from src.shared.bot_api.api import get_bot_api
+from src.shared.football_api.api import get_football_api
+
+
+def main() -> None:
+    db_api = get_bot_api()
+
+    for fixture in get_football_api().get_fixtures(today_only=False):
+        try:
+            db_api.save_or_update_fixture(fixture)
+        except BotApiError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/crons/ingest_fixture_events/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/root/WorldCupSweepstakesBot/venv/bin/python /root/WorldCupSweepstakesBot/src/crons/ingest_fixture_events
